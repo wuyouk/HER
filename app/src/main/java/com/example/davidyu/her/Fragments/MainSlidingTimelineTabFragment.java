a20a2c6a27ee8663e0f0b1b110c55dd210ca4387
caochao39 <caochao39@gmail.com>
2015-11-17 20:59:32 +0800

    filled timeline fragment

--- app/src/main/java/com/example/davidyu/her/Fragments/MainSlidingTimelineTabFragment.java
+++ app/src/main/java/com/example/davidyu/her/Fragments/MainSlidingTimelineTabFragment.java
@@ -5,9 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 
+import com.example.davidyu.her.Adapters.TimeLineAdapter;
 import com.example.davidyu.her.R;
+import com.example.davidyu.her.models.ChildEntity;
+import com.example.davidyu.her.models.GroupEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by David Yu on 6/5/2015.
@@ -19,6 +26,13 @@ public class MainSlidingTimelineTabFragment extends Fragment {
     static String[]descriptions={"Trignometry - Circumcentre","Price System","Chemical Bonding"};
     ListView listView;
 
+    private ExpandableListView expandableListView;
+    private List<GroupEntity> lists;
+    private TimeLineAdapter adapter;
+
+
+
+
     public static MainSlidingTimelineTabFragment getInstance(int position){
         MainSlidingTimelineTabFragment slidingTabFragment = new MainSlidingTimelineTabFragment();
         return slidingTabFragment;
@@ -26,15 +40,52 @@ public class MainSlidingTimelineTabFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View layout = inflater.inflate(R.layout.fragment_main_sliding_timeline_tab, container, false);
+        //View layout = inflater.inflate(R.layout.fragment_main_sliding_timeline_tab, container, false);
+        View layout = inflater.inflate(R.layout.timeline_main, container, false);
    //     adapter = new MainSlidingArchiveTabAdapter(getActivity(),Category,descriptions,qImages,getActivity());
    //     listView = (ListView) layout.findViewById(R.id.quetions_listview);
    //     listView.setAdapter(adapter);
+        expandableListView = (ExpandableListView)layout.findViewById(R.id.expandableListView);
+        initView();
         return layout;
 
     }
 
+    private void initView() {
+        lists=initList();
+        adapter=new TimeLineAdapter(this.getContext(),lists);
+        //expandableListView= (ExpandableListView)findViewById(R.id.expandableListView);
+        //expandableListView= (ExpandableListView)getActivity().findViewById(R.id.expandableListView);
+        expandableListView.setAdapter(adapter);
+        expandableListView.setGroupIndicator(null); // 去掉默认带的箭头
+        expandableListView.setSelection(0);// 设置默认选中项
+        // 遍历所有group,将所有项设置成默认展开
+        int groupCount = expandableListView.getCount();
+        for (int i = 0; i < groupCount; i++) {
+            expandableListView.expandGroup(i);
+        }
+    }
+    private List<GroupEntity> initList() {
 
+        List<GroupEntity> groupList;
+        //测试数据
+        String[] groupArray = new String[] { "9月1日","9月2日", "9月3日"};
+        String[][] childTimeArray = new String[][] {
+                { "测试数据1",  "测试数据2", "测试数据3" },
+                {  "测试数据4"}, { "测试数据5", "测试数据6" } };
+        groupList = new ArrayList<GroupEntity>();
+        for (int i = 0; i < groupArray.length; i++){
+            GroupEntity groupEntity = new GroupEntity(groupArray[i]);
+            List<ChildEntity> childList = new ArrayList<ChildEntity>();
+            for (int j = 0; j < childTimeArray[i].length; j++) {
+                ChildEntity childStatusEntity = new ChildEntity(childTimeArray[i][j]);
+                childList.add(childStatusEntity);
+            }
+            groupEntity.setChildEntities(childList);
+            groupList.add(groupEntity);
+        }
+        return groupList;
+    }
 
 
 }
